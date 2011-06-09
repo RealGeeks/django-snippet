b8cf023cf2d30640ed16b96d0dc2cd435104a6e2
Shu Zomg Chen <shu.chen@freelancedreams.com>
2011-06-08 23:09:54 -1000

    Oops, shouldn't be using tinymce here

--- snippet/models.py
+++ snippet/models.py
@@ -1,10 +1,9 @@
 from django.db import models
-from tinymce import models as tinymce_models
 
 # Create your models here.
 class Snippet(models.Model):
 	name = models.CharField(max_length=255)
-	content = tinymce_models.HTMLField(blank = True)
+	content = models.TextField(blank=True)
 	
 	def __unicode__(self):
 		return self.name

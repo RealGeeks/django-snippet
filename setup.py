13c01be1df327ea6d9c42feb68808f5f3c6e70ad
Shu Zomg Chen <shu.chen@freelancedreams.com>
2011-06-08 23:10:03 -1000

    Changed name to django-snippet

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-	name = 'django-snippets',
+	name = 'django-snippet',
 	version = '1.0',
 	#packages = ['snippet',],
 	packages = find_packages(),

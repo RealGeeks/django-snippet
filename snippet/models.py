90280dd42a0942222cb91099bd55311097141a22
Shu Zomg Chen <shu.chen@freelancedreams.com>
2011-06-08 18:45:28 -1000

    Initial code dump

---
+++ snippet/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+from django.utils.safestring import mark_safe
+from tinymce import models as tinymce_models
+
+# Create your models here.
+class Snippet(models.Model):
+	name = models.CharField(max_length=255)
+	content = tinymce_models.HTMLField(blank = True)
+	
+	def __unicode__(self):
+		return self.name
+		
